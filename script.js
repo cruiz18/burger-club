c906816b639c391f3b7e0b5b38ffdd9fef2918ad
cruiz18 <caruiz@cristoreyny.org>
2017-06-29 20:29:28 +0000

    almost finished

--- script.js
+++ script.js
@@ -3,24 +3,20 @@ $(document).ready(function() {
     /*$("#social").click(function() {
         $(".col-sm-6").animate({
             height: "300px"*/
-            
-            $("#social").hover(function();
-            {$(this).css("background-color:rgb(255,255,255)");
-            } , function(){
-              $(this).css("background-color:rgb(1,2,255)");
-            }
-              
-            
-        );
-        
-         $("#signUp").click(function() {
-             alert("Hi " + name + "thank you for signing up! We will send an email to " + email + " shortly.");
-         });
 
-        
+    $().hover(function() {
+            $(this).css("color:rgb(255,255,255)");
+        }, function() {
+            $(this).css("color:rgb(1,2,255)");
+        });
+    
+    $("#signUp").click(function() {
+        var name = $("#clientName").val();
+        var email = $("#clientEmail").val();
+        alert("Hi " + name + " thank you for signing up! We will send an email to " + email + " shortly.");
     });
 
-   
 
-var name = $("#clientName").val();
-var email = $("#clientEmail").val();
+});
+
+
