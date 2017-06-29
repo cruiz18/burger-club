06a36a177d8e6c6ecd1ae2a5117b11ba6add627c
cruiz18 <caruiz@cristoreyny.org>
2017-06-29 19:00:16 +0000

    added things

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 
-   
-    
+    /*$("#social").click(function() {
+        $(".col-sm-6").animate({
+            height: "300px"*/
+            
+            $("#social").hover(function();
+            {$(this).css("background-color:rgb(255,255,255)");
+            } , function(){
+              $(this).css("background-color:rgb(1,2,255)");
+            }
+              
+            
+        );
+        
+         $("#signUp").click(function() {
+             alert("Hi " + name + "thank you for signing up! We will send an email to " + email + " shortly.");
+         });
 
-});
+        
+    });
+
+   
+
+var name = $("#clientName").val();
+var email = $("#clientEmail").val();
