3416bebfbd032e0dd8f3498fd97eec442b24272b
cruiz18 <caruiz@cristoreyny.org>
2017-06-30 18:47:19 +0000

    finshed to best extent

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-//    $("#social1").hover(function() {
-//            $(this).css("background-color", "rgb(255,255,255)");
-//        }, function() {
-//            $(this).css("background-color", "rgb(1,2,255)");
-//        });
-    
-    
     // When hovering over the social media icons animate
     $('#social1').hover(function() {
         $("#social1").animate({
@@ -50,11 +43,25 @@ $(document).ready(function() {
     
     
     // The alert when a person signs up
+    
     $("#signUp").click(function() {
         var name = $("#clientName").val();
         var email = $("#clientEmail").val();
         alert("Hi " + name + " thank you for signing up! We will send an email to " + email + " shortly.");
     });
+    
+    //$("#signUp").click(function() {
+//         $('#thankyouModal').modal('show');
+  //      $('#thankyouModal').submit(function(e) {
+    //        e.preventDefault(); // don't submit multiple times
+      //      this.submit(); // use the native submit method of the form element
+        //    $('#thankyouModal').modal('show'); //Open the model
+    //    });        var name = $("#clientName").val();
+      //  var email = $("#clientEmail").val();
+        //document.getElementById("#textBody").innerHTML = "Hi " + name + " thank you for signing up! We will send an email to " + email + " shortly.";
+//    });
+
+
 
 
 });
