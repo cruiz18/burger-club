0d42391c4e441379b004312ee3524ac6a2da20bb
cruiz18 <caruiz@cristoreyny.org>
2017-06-30 14:26:27 +0000

    finishing touches

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 $(document).ready(function() {
-
-    /*$("#social").click(function() {
-        $(".col-sm-6").animate({
-            height: "300px"*/
-
-    $().hover(function() {
-            $(this).css("color:rgb(255,255,255)");
-        }, function() {
-            $(this).css("color:rgb(1,2,255)");
-        });
+//    $("#social1").hover(function() {
+//            $(this).css("background-color", "rgb(255,255,255)");
+//        }, function() {
+//            $(this).css("background-color", "rgb(1,2,255)");
+//        });
     
+    
+    // When hovering over the social media icons animate
+    $('#social1').hover(function() {
+        $("#social1").animate({
+            paddingTop: "10px"
+        }, 100, function() {
+            $("#social1").animate({
+                paddingTop: "1px"
+            }, 100)
+        });
+    });
+    
+    $('#social2').hover(function() {
+        $("#social2").animate({
+            paddingTop: "10px"
+        }, 100, function() {
+            $("#social2").animate({
+                paddingTop: "3px"
+            }, 100)
+        });
+    });
+    
+     $('#social3').hover(function() {
+        $("#social3").animate({
+            paddingTop: "10px"
+        }, 100, function() {
+            $("#social3").animate({
+                paddingTop: "3px"
+            }, 100)
+        });
+    });
+    
+    $('#social4').hover(function() {
+        $("#social4").animate({
+            paddingTop: "10px"
+        }, 100, function() {
+            $("#social4").animate({
+                paddingTop: "3px"
+            }, 100)
+        });
+    });
+    
+    
+    
+    // The alert when a person signs up
     $("#signUp").click(function() {
         var name = $("#clientName").val();
         var email = $("#clientEmail").val();
